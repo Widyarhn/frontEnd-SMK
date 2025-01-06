506fdff01a59207ee43d7fb7f6467a2978965909
tigin <mtiginsawala23@gmail.com>
2025-01-06 10:09:56 +0700

    update

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -15,7 +15,7 @@
     <meta name="author" content="Phoenixcoded" />
 
     <!-- [Favicon] icon -->
-    <link rel="icon" href="{{asset('assets')}}/images/logoapp.png" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('assets') }}/images/logoapp.png" type="image/x-icon" />
     <!-- [Font] Family -->
     <link rel="stylesheet" href="https://ableproadmin.com/assets/fonts/inter/inter.css" id="main-font-link" />
     <!-- [phosphor Icons] https://phosphoricons.com/ -->
@@ -32,6 +32,9 @@
     <link rel="stylesheet" href="https://ableproadmin.com/assets/css/style.css" id="main-style-link" />
     <script src="https://ableproadmin.com/assets/js/tech-stack.js"></script>
     <link rel="stylesheet" href="https://ableproadmin.com/assets/css/style-preset.css" />
+    <link rel="stylesheet" href="{{ asset('assets') }}/css/select2.min.css" />
+
+    <script src="{{ asset('assets') }}/js/plugins/jquery-3.7.1.min.js"></script>
 </head>
 <style>
     .welcome-banner::after {
@@ -71,13 +74,14 @@
                         </div>
 
                         <form id="wizard-form">
-                            <div class="wizard-step step-1">
+                            <div class="wizard-step step-1" data-step="1">
                                 <h5 class="text-left mb-4" style="color:#214f96;"><i
                                         class="fa-solid fa-circle-info fa-lg me-2"></i>Data Perusahaan</h5>
                                 <div class="mb-3">
                                     <label class="form-label fw-bold" for="nib">Nomor Induk Berusaha (NIB)</label>
                                     <div class="input-group">
-                                        <div class="input-group-text"><i class="fa-solid fa-magnifying-glass text-dark"></i></div>
+                                        <div class="input-group-text"><i
+                                                class="fa-solid fa-magnifying-glass text-dark"></i></div>
                                         <input type="text" class="form-control" id="nib"
                                             placeholder="Masukkan nomor induk berusaha" />
                                     </div>
@@ -88,17 +92,18 @@
                                     <label class="form-check-label" for="customCheckdefout2">Sinkronisasi dengan akun OSS jika ada</label>
                                 </div> --}}
                                 <div class="d-grid mt-5">
-                                    <button type="submit" class="btn btn-primary mb-2" style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Cek NIB</button>
+                                    <button type="submit" class="btn btn-primary mb-2"
+                                        style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Cek
+                                        NIB</button>
                                 </div>
                             </div>
-                            <div class="wizard-step step-2">
+                            <div class="wizard-step step-2" data-step="2">
                                 <h5 class="text-left mb-4" style="color:#214f96;"><i
                                         class="fa-solid fa-circle-info fa-lg me-2"></i>Data Perusahaan</h5>
                                 <div class="row g-4">
                                     <div class="col-md-6">
                                         <div class="form-floating mb-0">
-                                            <input type="text" class="form-control" id="nib"
-                                                placeholder="" />
+                                            <input type="text" class="form-control" id="nib" placeholder="" />
                                             <label for="nib">NIB</label>
                                         </div>
                                     </div>
@@ -133,33 +138,27 @@
                                 <div class="row g-4">
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <div class="form-floating">
-                                                <select class="form-select" id="floatingSelect"
-                                                    aria-label="Floating label select example">
-                                                    <option selected>Pilih Provinsi</option>
-                                                    <option value="1">Jabar</option>
-                                                    <option value="2">Jatim</option>
-                                                    <option value="3">Banten</option>
-                                                </select>
-                                                <label for="floatingSelect">Provinsi</label>
-                                            </div>
+                                            <label class="fw-normal" for="input-provinsi">Provinsi</label>
+                                            <select class="form-control select2" name="input_provinsi"
+                                                id="input-provinsi">
+                                                <option value="" disabled selected>Pilih Provinsi</option>
+                                                <option value="1">Jawa Tengah</option>
+                                                <option value="2">Jawa Barat</option>
+                                            </select>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="mb-3">
-                                            <div class="form-floating">
-                                                <select class="form-select" id="floatingSelect"
-                                                    aria-label="Floating label select example">
-                                                    <option selected>Pilih Kota</option>
-                                                    <option value="1">Jabar</option>
-                                                    <option value="2">Jatim</option>
-                                                    <option value="3">Banten</option>
-                                                </select>
-                                                <label for="floatingSelect">Kota/Kabupaten</label>
-                                            </div>
+                                            <label class="fw-normal" for="input-kota">Kota</label>
+                                            <select class="form-control select2" name="input_kota" id="input-kota">
+                                                <option value="" disabled selected>Pilih Kota</option>
+                                                <option value="1">Semarang</option>
+                                                <option value="2">Bandung</option>
+                                            </select>
                                         </div>
                                     </div>
                                 </div>
+
                                 <div class="mb-3">
                                     <div class="col-md-12">
                                         <div class="form-floating mb-0">
@@ -170,23 +169,18 @@
                                 </div>
                                 <div class="mb-3">
                                     <div class="col-md-12">
-                                        <div class="form-floating">
-                                            <select class="form-select" id="floatingSelect"
-                                                aria-label="Floating label select example">
-                                                <option selected>Pilih Jenis Pelayanan</option>
-                                                <option value="1">Jabar</option>
-                                                <option value="2">Jatim</option>
-                                                <option value="3">Banten</option>
-                                            </select>
-                                            <label for="floatingSelect">Jenis Pelayanan</label>
-                                        </div>
+                                        <label class="fw-normal" for="input-pelayanan">Jenis Pelayanan</label>
+                                        <select class="form-control select2" name="input_pelayanan"
+                                            id="input-pelayanan">
+                                        </select>
                                     </div>
                                 </div>
                                 <div class="d-grid mt-5">
-                                    <button type="submit" class="btn btn-primary mb-2" style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Selanjutnya</button>
+                                    <button type="submit" class="btn btn-primary mb-2"
+                                        style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Selanjutnya</button>
                                 </div>
                             </div>
-                            <div class="wizard-step step-3" style="display: none;">
+                            <div class="wizard-step step-3" data-step="3" style="display: none;">
                                 <h5 class="text-left mb-4" style="color:#214f96;"><i
                                         class="fa-solid fa-circle-info fa-lg me-2"></i>Penanggung Jawab (PIC)</h5>
                                 <div class="row g-4">
@@ -210,11 +204,13 @@
                                     </div>
                                 </div>
                                 <div class="d-grid mt-5">
-                                    <button type="submit" class="btn btn-primary mb-2" style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Selanjutnya</button>
-                                    <button type="button" class="btn btn-outline-secondary prev-btn" style="border-radius:5px;">Kembali</button>
+                                    <button type="submit" class="btn btn-primary mb-2"
+                                        style="border-radius:5px; background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;">Selanjutnya</button>
+                                    <button type="button" class="btn btn-outline-secondary prev-btn"
+                                        style="border-radius:5px;">Kembali</button>
                                 </div>
                             </div>
-                            <div class="wizard-step step-4" style="display: none;">
+                            <div class="wizard-step step-4" data-step="4" style="display: none;">
                                 <h5 class="text-left mb-4" style="color:#214f96;"><i
                                         class="fa-solid fa-circle-info fa-lg me-2"></i>Informasi Akun</h5>
                                 <div class="row g-4">
@@ -247,7 +243,8 @@
                                     </div>
                                 </div>
                                 <div class="d-grid mt-5">
-                                    <a href="/admin/dashboard" class="btn mb-2" style="border-radius:5px;background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;"">Simpan</a>
+                                    <a href="/admin/dashboard" class="btn mb-2"
+                                        style="border-radius:5px;background: linear-gradient(90deg, rgb(4 60 132) 0%, rgb(69 114 184) 100%); color: white;"">Simpan</a>
                                 </div>
                             </div>
                         </form>
@@ -270,9 +267,160 @@
     <script src="https://ableproadmin.com/assets/js/fonts/custom-font.js"></script>
     <script src="https://ableproadmin.com/assets/js/pcoded.js"></script>
     <script src="https://ableproadmin.com/assets/js/plugins/feather.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/js-cookie/2.1.3/js.cookie.js"></script>
+    <script src="{{ asset('assets') }}/js/select2/select2.full.min.js"></script>
+    <script src="{{ asset('assets') }}/js/select2/select2.min.js"></script>
+    <script src="{{ asset('assets') }}/js/axios.js"></script>
+    <script src="{{ asset('assets') }}/js/restAPI.js"></script>
     <script>
+        async function checkOSS() {
+            try {
+                // Menunggu hasil dari CallAPI
+                const getDataRest = await CallAPI('GET', '/dummy/check_oss.json');
+
+                // Mengecek status response
+                if (getDataRest.status === 200) {
+                    const active = getDataRest.data.data.is_active; // Mengambil nilai is_active
+
+                    if (active === false) {
+                        adjustWizardSteps(); // Langsung ke step-2 jika active === false
+                    }
+                }
+            } catch (error) {
+                // Menangani error
+                if (error.response) {
+                    console.error('Error response:', error.response);
+                } else {
+                    console.error('Error:', error.message);
+                }
+            }
+        }
+
+        function adjustWizardSteps() {
+            // Menyembunyikan semua langkah
+            const steps = document.querySelectorAll('.wizard-step');
+            steps.forEach((step) => {
+                step.style.display = 'none';
+            });
+
+            // Menampilkan langkah kedua (step-2)
+            const step2 = document.querySelector('.wizard-step.step-2');
+            if (step2) {
+                step2.style.display = 'block'; // Tampilkan step-2
+            } else {
+                console.error('Step 2 element not found in DOM.');
+            }
+        }
+
+        function selectFilter(id) {
+            if (id === '#input-provinsi') {
+                $(id).select2({
+                    ajax: {
+                        url: `/dummy/provinsi.json`,
+                        dataType: 'json',
+                        delay: 500,
+                        headers: {
+                            Authorization: `Bearer ${Cookies.get('auth_token')}`
+                        },
+                        data: function(params) {
+                            let query = {
+                                search: params.term,
+                                page: 1,
+                                limit: 30,
+                                ascending: 1
+                            }
+                            return query;
+                        },
+                        processResults: function(res) {
+                            let data = res.data;
+                            return {
+                                results: $.map(data, function(item) {
+                                    return {
+                                        id: item.id,
+                                        text: item.nama
+                                    }
+                                })
+                            };
+                        },
+                    },
+                    allowClear: true,
+                    placeholder: 'Pilih perusahaan',
+                    width: '100%' // Menyesuaikan lebar elemen Select2 dengan container
+                });
+            } else if (id === '#input-kota') {
+                $(id).select2({
+                    ajax: {
+                        url: `/dummy/kota.json`,
+                        dataType: 'json',
+                        delay: 500,
+                        headers: {
+                            Authorization: `Bearer ${Cookies.get('auth_token')}`
+                        },
+                        data: function(params) {
+                            let query = {
+                                search: params.term,
+                                page: 1,
+                                limit: 30,
+                                ascending: 1
+                            }
+                            return query;
+                        },
+                        processResults: function(res) {
+                            let data = res.data;
+                            return {
+                                results: $.map(data, function(item) {
+                                    return {
+                                        id: item.id,
+                                        text: item.nama
+                                    }
+                                })
+                            };
+                        },
+                    },
+                    allowClear: true,
+                    placeholder: 'Pilih perusahaan',
+                    width: '100%' // Menyesuaikan lebar elemen Select2 dengan container
+                });
+            } else if (id === '#input-pelayanan') {
+                $(id).select2({
+                    ajax: {
+                        url: `/dummy/service_type.json`,
+                        dataType: 'json',
+                        delay: 500,
+                        headers: {
+                            Authorization: `Bearer ${Cookies.get('auth_token')}`
+                        },
+                        data: function(params) {
+                            let query = {
+                                search: params.term,
+                                page: 1,
+                                limit: 30,
+                                ascending: 1
+                            }
+                            return query;
+                        },
+                        processResults: function(res) {
+                            let data = res.data;
+                            return {
+                                results: $.map(data, function(item) {
+                                    return {
+                                        id: item.id,
+                                        text: item.name
+                                    }
+                                })
+                            };
+                        },
+                    },
+                    allowClear: true,
+                    placeholder: 'Pilih jenis pelayanan',
+                    width: '100%' // Menyesuaikan lebar elemen Select2 dengan container
+                });
+            }
+        }
+
         document.addEventListener("DOMContentLoaded", () => {
             const steps = document.querySelectorAll(".wizard-step");
+            const wizardContainer = document.querySelector(".wizard-container");
             let currentStep = 0;
 
             const showStep = (index) => {
@@ -305,6 +453,14 @@
 
             // Tampilkan langkah pertama saat pertama kali halaman dimuat
             showStep(currentStep);
+
+            // Panggil fungsi checkOSS secara otomatis
+            checkOSS();
+
+            // Inisialisasi Select2
+            selectFilter('#input-provinsi');
+            selectFilter('#input-kota');
+            selectFilter('#input-pelayanan');
         });
     </script>
 
